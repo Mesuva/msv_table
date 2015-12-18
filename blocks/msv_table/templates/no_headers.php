f7c6a5d2d7357b6e917c25f429918f8beca1e4b7
avdevs <bhavin@avdevs.com>
2015-12-18 18:23:32 +0530

    Add bold, highlighted css class and responsive table class
    Add highlighted, bold css class for cell and add responsive table class.

--- blocks/msv_table/templates/no_headers.php
+++ blocks/msv_table/templates/no_headers.php
@@ -44,7 +44,8 @@ if (!empty($table_data)): ?>
         if (empty($data)) {
             $table = '';
         } else {
-            $table = '<table class="table table-bordered">';
+            $table = '<div class="table-responsive">';
+			$table .= '<table class="table table-bordered">';
 
             if ($header) {
                 $table .= '<thead>';
@@ -71,7 +72,7 @@ if (!empty($table_data)): ?>
                 }
 
                 foreach($row as $val) {
-                    $val =  trim($th->entities($val));
+                    //$val =  trim($th->entities($val));// add in comment to display HTML content like image
                     $empty = '';
 
                     if ($val == '') {
@@ -132,6 +133,7 @@ if (!empty($table_data)): ?>
             }
 
             $table .= '</table>';
+            $table .= '</div>';
         }
 
         echo $table;
@@ -161,6 +163,21 @@ if (!empty($table_data)): ?>
     .table_display .htMiddle {
         vertical-align: middle;
     }
+    .table_display .htBold{
+        	font-weight: bold;
+    	}
+    	.table_display td.highlightedRed{
+        	background: red;
+        	color: white;
+    	}
+    	.table_display td.highlightedGreen{
+        	background: green;
+        	color: white;
+    	}
+    	.table_display td.highlightedOrange{
+        	background: orange;
+        	color: white;
+    	}
 </style>
 
 
